05d6dd0736004f9fa3a9ed10ae3ec3f95659d1ed
Julia Zapata <juliamzapata@gmail.com>
2018-09-30 20:01:55 -0300

    ultimo commit, tudo funcionando

--- dist/routes.js
+++ dist/routes.js
@@ -1,14 +1,17 @@
 $(document).ready(()=>{
-    page("/", index);
-    page('/characters', choosedChacarter);
-    page();
-
-    function index(){
-        return document.getElementById("showed-characters").innerHTML = `<p>oi</p>`;
-    }
-    function choosedChacarter(){
-        // bananinha();
-        putCharacters();
-        // return document.getElementById("showed-characters").innerHTML = `<p>que cocozão</p>`;
-    }
+  page("/", index);
+  page('/characters', choosedChacarter);
+  page('/comics', choosedComic);
+  page();
+  function index(){
+    $("#showed-informartion").html(`<div class="first-index-style"><span class="site-phrase"><h2>Find your favorite characters and comics in the</h2><h2 class="site-phrase-marvel d-flex justify-content-center">Marvel's Universe!</h2></span>
+    <div class="dared-evil"></div>
+    </div>`); 
+  }
+  function choosedChacarter(){
+    putCharacters();
+  }
+  function choosedComic(){
+    putComics();
+  }
 });
